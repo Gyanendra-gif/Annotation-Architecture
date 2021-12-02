58c77978f5032b863b87171bab6f3891168292a0
Gyanendra Pratap Singh Sengar <[gyanendra1252@gmail.com]>
2021-12-02 11:09:38 +0530

    [Gyanendra] Add: A Demo Program of Annotation

---
+++ Annotation/Annotation/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Annotation
+{
+    class Employee
+    {
+        [Required(ErrorMessage = "Employee {0} is required")]
+        [StringLength (100, MinimumLength = 3, ErrorMessage = "Name Should be minimum 3 characters and maximim 100 characters")]
+        [DataType(DataType.Text)]
+        public string Name { get; set; }
+        [Range(18, 99, ErrorMessage = "Age Should be Above 18")]
+        public int Age { get; set; }
+        [DataType(DataType.PhoneNumber)]
+        [Phone]
+        public string PhoneNumber { get; set; }
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
